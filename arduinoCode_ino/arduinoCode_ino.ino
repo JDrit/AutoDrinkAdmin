d2324f7bac3c758150cdf7af71eb3ef72e41e7c7
Joseph Batchik <jd@ultrabook.student.rit.edu>
2013-10-14 15:19:23 -0400

    added inbhibting code to the ardunio

--- arduinoCode_ino/arduinoCode_ino.ino
+++ arduinoCode_ino/arduinoCode_ino.ino
@@ -5,14 +5,20 @@ OneWire ds(4); // One Wire for iButton on pin 4
 
 unsigned long int time;             // the current time unit
 unsigned long timeOfLastPulseCoins; // the time of the last pulse from the coin reader
-volatile int coinsValue;            // the value for the coin reader
-int coinsChange;                    // 1 if the coin has had input, 0 otherwise
+unsigned long timeOfLastPulseBills; // the time of the last pulse from the bill reader
+unsigned long heartBeat;            // the time of the last heart beat from the laptop to make sure the computer is still active
+volatile int coinsValue;            // the amount of money from the coin reader
+volatile int billsValue;            // the amount of money from the bill reader
+volatile int coinsChange;           // 1 if the coin reader has had input, 0 otherwise
+volatile int billsChange;           // 1 if the bill reader has had input, 0 otherwise
 String result;                      // result from the iButton reader
 byte ibutton_id[IBUTTON_ID_SIZE];   // data to hold the iButton ID
 
 void setup()
 {
   timeOfLastPulseCoins = millis();
+  timeOfLastPulseBills = millis();
+  heartBeat = millis();
   coinsChange = 0;
   coinsValue = 0;
   Serial.begin(9600);
@@ -20,6 +26,8 @@ void setup()
   delay(500);
   digitalWrite(9, LOW);
   attachInterrupt(0, coinInserted, RISING);
+  attachInterrupt(1, billInserted, FALLING);
+  digitalWrite(3, HIGH);
 }
 
 /*
@@ -31,13 +39,46 @@ void coinInserted() {
   timeOfLastPulseCoins = millis();
 }
 
+/*
+ * interupt method for when the bill reader pulses
+ */
+void billInserted() {
+  billsValue++;
+  billsChange = 1;
+  timeOfLastPulseBills = millis();
+}
+
+/*
+ * Sends the command to the readers to stop accepting money
+ */
+void inhibitReaders() {
+  
+}
+
+/*
+ * Sends the command to the readers to start accepting money
+ */
+void startReaders() {
+  
+}
+
 void loop()
 {
+  String input = "";
   
   if (Serial.available()) { // if the computer writes to the arduino
-    Serial.println(Serial.read());
+    input = Serial.read();
+    if (input == "l") { // user logged out
+      inhibitReaders();
+    } else if (input == "a") { // user was authenticated
+      startReaders()
+    } else if (input == "h") { // laptop sent heart beat
+       heartBeat = millis();
+    }
   }
-  
+  if (millis() - heartBeart > 1000) {
+    startReaders();
+  }
   // clears & initialzes the iButton buffer
   for (int i = 0 ; i < IBUTTON_ID_SIZE ; i++) {
     ibutton_id[i] = 0;
@@ -52,7 +93,7 @@ void loop()
     }
     Serial.print("i:" + result);
     delay(1000);
-    result = '';
+    result = "";
   } else {
     ds.reset_search();
   }
@@ -61,6 +102,11 @@ void loop()
     coinsChange = 0;
     Serial.print("m:" + String(coinsValue));
     coinsValue = 0;
+  }
+  if (billsChange == 1 && millis() - timeOfLastPulseBills > 1000) {
+    billsChange = 0;
+    Serial.print("m:" + String(billsValue * 100));
+    billsValue = 0;
   }
   
 }
