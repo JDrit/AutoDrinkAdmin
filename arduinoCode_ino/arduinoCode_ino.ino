54168ee6301d4acd4709daeedd1eeff3fa1710da
jd <jd@csh.rit.edu>
2013-10-25 13:19:43 -0400

    Added new arduino code

--- arduinoCode_ino/arduinoCode_ino.ino
+++ arduinoCode_ino/arduinoCode_ino.ino
@@ -6,16 +6,15 @@ OneWire ds(4); // One Wire for iButton on pin 4
 unsigned long int time;             // the current time unit
 unsigned long timeOfLastPulseCoins; // the time of the last pulse from the coin reader
 unsigned long timeOfLastPulseBills; // the time of the last pulse from the bill reader
-<<<<<<< HEAD
 unsigned long heartBeat;            // the time of the last heart beat from the laptop to make sure the computer is still active
-=======
->>>>>>> a105287c809ab453c80edc32efbbbd5880d22b9a
 volatile int coinsValue;            // the amount of money from the coin reader
 volatile int billsValue;            // the amount of money from the bill reader
 volatile int coinsChange;           // 1 if the coin reader has had input, 0 otherwise
 volatile int billsChange;           // 1 if the bill reader has had input, 0 otherwise
 String result;                      // result from the iButton reader
 byte ibutton_id[IBUTTON_ID_SIZE];   // data to hold the iButton ID
+volatile unsigned long highTime;    // the time of a high pulse from the coin reader
+int coinReader = 2; 
 
 void setup()
 {
@@ -26,20 +25,28 @@ void setup()
   coinsValue = 0;
   Serial.begin(9600);
   pinMode(9,OUTPUT);
+  pinMode(7,OUTPUT); // the pin used for the inhibiter
   delay(500);
   digitalWrite(9, LOW);
-  attachInterrupt(0, coinInserted, RISING);
+  attachInterrupt(0, coinInserted, CHANGE);
   attachInterrupt(1, billInserted, FALLING);
-  digitalWrite(3, HIGH);
+  digitalWrite(3, HIGH); // bill reader
 }
 
 /*
- * interupt method for when the coin reader pulses
+ * interupt method for when the coin reader pulses. This only gets the pulses that are less
+ * than 150 milliseconds
  */
 void coinInserted() {
-  coinsValue++;
-  coinsChange = 1;
-  timeOfLastPulseCoins = millis();
+  if (digitalRead( coinReader ) == HIGH) {
+    highTime = millis(); //get time of pulse going HIGH
+   } else { 
+    if (millis() - highTime < 150) { // if the pulse width was less than 150, so no start up pulses
+      coinsValue++;
+      coinsChange = 1;
+      timeOfLastPulseCoins = millis();
+    }
+  }
 }
 
 /*
@@ -51,39 +58,40 @@ void billInserted() {
   timeOfLastPulseBills = millis();
 }
 
-<<<<<<< HEAD
 /*
  * Sends the command to the readers to stop accepting money
  */
 void inhibitReaders() {
-  
+  digitalWrite(7, LOW);
 }
 
 /*
  * Sends the command to the readers to start accepting money
  */
 void startReaders() {
-  
+  digitalWrite(7, HIGH);
 }
 
-=======
->>>>>>> a105287c809ab453c80edc32efbbbd5880d22b9a
 void loop()
 {
-  String input = "";
+  char input;
   
   if (Serial.available()) { // if the computer writes to the arduino
     input = Serial.read();
-    if (input == "l") { // user logged out
+    if (input == 'l') { // user logged out
       inhibitReaders();
-    } else if (input == "a") { // user was authenticated
-      startReaders()
-    } else if (input == "h") { // laptop sent heart beat
+    } else if (input == 'a') { // user was authenticated
+      startReaders();
+    } else if (input == 'h') { // laptop sent heart beat
        heartBeat = millis();
+       startReaders();
+    } else {
+      inhibitReaders();
     }
   }
-  if (millis() - heartBeart > 1000) {
-    startReaders();
+
+  if (millis() - heartBeat > 1000) {
+//    inhibitReaders();
   }
   // clears & initialzes the iButton buffer
   for (int i = 0 ; i < IBUTTON_ID_SIZE ; i++) {
@@ -104,14 +112,14 @@ void loop()
     ds.reset_search();
   }
   
-  if (coinsChange == 1 && millis() - timeOfLastPulseCoins > 1000) {
+  if (coinsChange == 1 && millis() - timeOfLastPulseCoins > 200) {
     coinsChange = 0;
-    Serial.print("m:" + String(coinsValue));
+    Serial.print("c:" + String(coinsValue));
     coinsValue = 0;
   }
-  if (billsChange == 1 && millis() - timeOfLastPulseBills > 1000) {
+  if (billsChange == 1 && millis() - timeOfLastPulseBills > 500) {
     billsChange = 0;
-    Serial.print("m:" + String(billsValue * 100));
+    Serial.print("d:" + String(billsValue * 100));
     billsValue = 0;
   }
   
